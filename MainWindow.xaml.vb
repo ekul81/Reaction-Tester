32d8deb0eb69a815cf95277af4558251f8526e82
Luke Dickety <Luke.dickety@rocketmail.com>
2012-02-07 21:30:02 +0000

    now works (-:

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,6 +1,7 @@
 ﻿Class MainWindow 
-    Dim Timer As System.Timers.Timer
+    Dim Timer As New System.Diagnostics.Stopwatch
     Dim watcherthread As System.Threading.Thread
+    Dim sresults As String = ""
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles Button1.Click
         Dim thr As New System.Threading.Thread(AddressOf watchersub)
         thr.Start()
@@ -8,12 +9,14 @@
     End Sub
 
     Sub watchersub()
+        Timer.Reset()
         Dim r As New Random
         Randomize()
         Dim Icount As Integer = r.Next(5000)
         Icount += 500
         System.Threading.Thread.Sleep(Icount)
-        grid.Background.Dispatcher.Invokestart(changecolour)
+
+        Dispatcher.BeginInvoke(New Action(AddressOf changecolour), System.Windows.Threading.DispatcherPriority.Render)
         Timer.Start()
     End Sub
     'Sub changecolour()
@@ -23,10 +26,16 @@
         Return (Nothing)
     End Function
 
-    Private Sub grid_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Input.KeyEventArgs) Handles grid.KeyDown, Button1.KeyDown
+    Private Sub grid_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Input.KeyEventArgs) Handles grid.KeyDown, Button1.KeyDown, MyBase.KeyDown
         If e.Key = Key.Space Then
             Timer.Stop()
-            Timer.Interval = Timer.Interval
+            Dim newinteger As Integer = Timer.ElapsedMilliseconds
+            newinteger = newinteger
+            Button1.Visibility = Windows.Visibility.Visible
+            sresults += newinteger.ToString + ", "
+            grid.Background = New System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Blue)
+            Button1.Content = "again? " + vbNewLine + sresults
+
         End If
     End Sub
 End Class
